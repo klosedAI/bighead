df57f997ebead09fa2ca027d6cb46d90797dcd82
gatech <23293632+pratiksha-pai@users.noreply.github.com>
2023-11-18 21:09:51 -0500

    update chatwindow initial

--- scripts/chatWindow.js
+++ scripts/chatWindow.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const sendButton = document.getElementById('send-button');
+    const chatInput = document.getElementById('chat-input');
+    const chatHistory = document.getElementById('chat-history');
     const closeButton = document.getElementById('close-button');
+
+    let conversationHistory = '';
 
     closeButton.addEventListener('click', function () {
         // Replace the chat window with the chatButton iframe
@@ -14,4 +19,21 @@ document.addEventListener('DOMContentLoaded', function () {
         // Append the chatButton iframe to the body
         document.body.appendChild(chatButtonIframe);
     });
+
+    sendButton.addEventListener('click', function () {
+        const userInput = chatInput.value;
+        if (userInput) {
+            updateChatHistory('User', userInput);
+            // Send user input to the API along with scraped data
+            sendDataToAPI(userInput, (response) => {
+                updateChatHistory('Bot', response);
+            });
+        }
+        chatInput.value = ''; // Clear input field after sending
+    });
+
+    function updateChatHistory(sender, message) {
+        conversationHistory += `<p><strong>${sender}:</strong> ${message}</p>`;
+        chatHistory.innerHTML = conversationHistory;
+    }
 });
